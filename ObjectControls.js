985b6b944ad9acacd634e71f00b9f912f18b52e9
Alberto Piras <apiras@google.com>
2019-05-17 11:05:31 +0200

    Update existing code comments

--- ObjectControls.js
+++ ObjectControls.js
@@ -15,10 +15,8 @@ license: MIT
  * @param objectToMove - the object to control.
  */
 
-
 THREE.ObjectControls = function(camera, domElement, objectToMove) {
-
-	mesh = objectToMove;
+  mesh = objectToMove;
   domElement = (domElement !== undefined) ? domElement : document;
 
   this.setObjectToMove = function(newMesh) {
@@ -36,9 +34,9 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
 
   this.setRotationSpeed = function(newRotationSpeed) {
     rotationSpeed = newRotationSpeed;
-	};
+  };
 
-	this.setRotationSpeedTouchDevices = function(newRotationSpeed) {
+  this.setRotationSpeedTouchDevices = function(newRotationSpeed) {
     rotationSpeedTouchDevices = newRotationSpeed;
   };
 
@@ -76,22 +74,20 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
 
   this.disableMaxVerticalAngleRotation = function() {
     MAX_ROTATON_ANGLES.x.enabled = false;
-	};
+  };
 
-  /************* MOUSE Interaction Controls (rotate & zoom, desktop)
-   * ***************/
-  // MOUSE - move
+  /** Mouse Interaction Controls (rotate & zoom, desktop **/
+  // Mouse - move
   domElement.addEventListener('mousedown', mouseDown, false);
   domElement.addEventListener('mousemove', mouseMove, false);
   domElement.addEventListener('mouseup', mouseUp, false);
 
-  // MOUSE - zoom
+  // Mouse - zoom
   domElement.addEventListener('wheel', wheel, false);
 
 
-  /************** TOUCH Interaction Controls (rotate & zoom, mobile)
-   * ************/
-  // TOUCH - move
+  /** Touch Interaction Controls (rotate & zoom, mobile) **/
+  // Touch - move
   domElement.addEventListener('touchstart', onTouchStart, false);
   domElement.addEventListener('touchmove', onTouchMove, false);
   domElement.addEventListener('touchend', onTouchEnd, false);
@@ -100,25 +96,26 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
 
   var MAX_ROTATON_ANGLES = {
     x: {
-      // VERTICAL from bottom to top
+      // Vertical from bottom to top.
       enabled: false,
       from: Math.PI / 8,
       to: Math.PI / 8
     },
     y: {
-      // HORIZONTAL from left to right
+      // Horizontal from left to right.
       enabled: false,
       from: Math.PI / 4,
       to: Math.PI / 4
     }
   };
 
-  // rotationSpeed
-  // 1= fast
-  // 0.01 = slow
-
+  /**
+   * RotationSpeed
+   * 1= fast
+   * 0.01 = slow
+   * */
   var maxDistance = 15, minDistance = 6, zoomSpeed = 0.5, rotationSpeed = 0.05,
-     rotationSpeedTouchDevices = 0.05, verticalRotationEnabled = false,
+      rotationSpeedTouchDevices = 0.05, verticalRotationEnabled = false,
       horizontalRotationEnabled = true;
 
   var mouseFlags = {MOUSEDOWN: 0, MOUSEMOVE: 1};
@@ -127,7 +124,8 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
   var isDragging = false;
   var previousMousePosition = {x: 0, y: 0};
 
-  /**currentTouches
+  /**
+   * CurrentTouches
    * length 0 : no zoom
    * length 2 : is zoomming
    */
@@ -146,7 +144,8 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
   }
 
   /**
-   *
+   * @description Checks if the rotation in a specific axe is within the maximum
+   * values allowed.
    * @param delta is the difference of the current rotation angle and the
    *     expected rotation angle
    * @param axe is the axe of rotation: x(vertical rotation), y (horizontal
@@ -162,7 +161,7 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
       return condition ? true : false;
     }
     return true;
-	}
+  }
 
   function resetMousePosition() {
     previousMousePosition = {x: 0, y: 0};
@@ -212,8 +211,8 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
     resetMousePosition();
   }
 
- function wheel(e) {
-    var delta = e.wheelDelta? e.wheelDelta : e.deltaY*-1;
+  function wheel(e) {
+    var delta = e.wheelDelta ? e.wheelDelta : e.deltaY * -1;
     if (delta > 0 && camera.position.z > minDistance) {
       zoomIn();
     } else if (delta < 0 && camera.position.z < maxDistance) {
@@ -232,18 +231,18 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
     } else {
       previousMousePosition = {x: e.touches[0].pageX, y: e.touches[0].pageY};
     }
-    // console.info("onTouchStart");
   }
 
   function onTouchEnd(e) {
     prevZoomDiff.X = null;
     prevZoomDiff.Y = null;
 
-    // if you were zooming out, currentTouches is updated for each finger you
-    // leave up the screen so each time a finger leaves up the screen,
-    // currentTouches length is decreased of a unit. When you leave up both 2
-    // fingers, currentTouches.length is 0, this means the zoomming phase is
-    // ended
+    /* If you were zooming out, currentTouches is updated for each finger you
+     * leave up the screen so each time a finger leaves up the screen,
+     * currentTouches length is decreased of a unit. When you leave up both 2
+     * fingers, currentTouches.length is 0, this means the zoomming phase is
+     * ended.
+     */
     if (currentTouches.length > 0) {
       currentTouches.pop();
     } else {
@@ -251,49 +250,44 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
     }
     e.preventDefault();
     if (flag === mouseFlags.MOUSEDOWN) {
-      // console.info("touchClick");
-      // you can invoke more other functions for animations and so on...
+      // TouchClick
+      // You can invoke more other functions for animations and so on...
     } else if (flag === mouseFlags.MOUSEMOVE) {
-      // console.info("touch drag");
-      // you can invoke more other functions for animations and so on...
+      // Touch drag
+      // You can invoke more other functions for animations and so on...
     }
-		// console.info("onTouchEnd");
-		resetMousePosition();
+    resetMousePosition();
   }
 
   function onTouchMove(e) {
     e.preventDefault();
     flag = mouseFlags.MOUSEMOVE;
-		// TOUCH ZOOM
-		// If two pointers are down, check for pinch gestures
+    // Touch zoom.
+    // If two pointers are down, check for pinch gestures.
     if (e.touches.length === 2) {
       currentTouches = new Array(2);
-      // console.info("onTouchZoom");
-      // Calculate the distance between the two pointers
+      // Calculate the distance between the two pointers.
       var curDiffX = Math.abs(e.touches[0].clientX - e.touches[1].clientX);
       var curDiffY = Math.abs(e.touches[0].clientY - e.touches[1].clientY);
 
       if (prevZoomDiff && prevZoomDiff.X > 0 && prevZoomDiff.Y > 0) {
         if ((curDiffX > prevZoomDiff.X) && (curDiffY > prevZoomDiff.Y) &&
             (camera.position.z > minDistance)) {
-          // console.info("Pinch moving IN -> Zoom in", e);
           zoomIn();
         } else if (
             curDiffX < prevZoomDiff.X && camera.position.z < maxDistance &&
             curDiffY < prevZoomDiff.Y) {
-          // console.info("Pinch moving OUT -> Zoom out", e);
           zoomOut();
         }
       }
-      // Cache the distance for the next move event
+      // Cache the distance for the next move event.
       prevZoomDiff.X = curDiffX;
       prevZoomDiff.Y = curDiffY;
 
-      // TOUCH ROTATE
+      // Touch Rotate.
     } else if (currentTouches.length === 0) {
       prevZoomDiff.X = null;
       prevZoomDiff.Y = null;
-      // console.info("onTouchMove");
       var deltaMove = {
         x: e.touches[0].pageX - previousMousePosition.x,
         y: e.touches[0].pageY - previousMousePosition.y
@@ -301,17 +295,18 @@ THREE.ObjectControls = function(camera, domElement, objectToMove) {
       previousMousePosition = {x: e.touches[0].pageX, y: e.touches[0].pageY};
 
       if (horizontalRotationEnabled && deltaMove.x != 0) {
-        if (!isWithinMaxAngle(Math.sign(deltaMove.x) * rotationSpeedTouchDevices, 'y'))
+        if (!isWithinMaxAngle(
+                Math.sign(deltaMove.x) * rotationSpeedTouchDevices, 'y'))
           return;
         mesh.rotation.y += Math.sign(deltaMove.x) * rotationSpeedTouchDevices;
       }
 
       if (verticalRotationEnabled && deltaMove.y != 0) {
-        if (!isWithinMaxAngle(Math.sign(deltaMove.y) * rotationSpeedTouchDevices, 'x'))
+        if (!isWithinMaxAngle(
+                Math.sign(deltaMove.y) * rotationSpeedTouchDevices, 'x'))
           return;
         mesh.rotation.x += Math.sign(deltaMove.y) * rotationSpeedTouchDevices;
       }
     }
-	}
-
+  }
 };
